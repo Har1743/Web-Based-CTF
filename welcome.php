e9f1908c0ea0283fbfc579a1341ff107b4186f2f
Hardik Chugh <42953153+Har1743@users.noreply.github.com>
2020-10-02 16:05:07 +0000

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -12,7 +12,7 @@ if (isset($_SESSION["user"]) && $_SESSION["user"] == "persia")
 	echo "<body style='background-color:lightblue'>";
 	echo "<br>";
 	echo "<h1 style='text-align:center;color:red'>Hi " . $user . "</h1>";
-	echo "<center>" . "<img src='mona.jpeg' alt='Trulli' width='900' height='400'>" . "</center>";
+	echo "<center>" . "<img src='monaa.jpeg' alt='Trulli' width='900' height='400'>" . "</center>";
 	echo "<p style='text-align:center'><b>you might wonder about the presence of this ->mona_lisa<- image</b></p>";
 	echo "<p style='text-align:center'>maybe this image helps you in towards to your next step</p>";
 	echo "</body>";
